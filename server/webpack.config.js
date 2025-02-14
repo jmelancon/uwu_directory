cabb3bd9256830919fa82c62cf9c202cf501f09d
Joseph Melancon <joseph@jmelancon.com>
2025-02-14 01:22:54 +0000

    Revert "Maybe make webpack encore work better with custom path (reverse proxy)"
    This reverts commit 638e6ea2729c920289c9262252caf5cfee5c284a.

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,10 +1,4 @@
 const Encore = require('@symfony/webpack-encore');
-const dotenv = require('dotenv');
-
-const env = dotenv.config();
-if (env.error) {
-    throw env.error;
-}
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -14,7 +8,7 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 
 Encore
     .setOutputPath('public/build/')
-    .setPublicPath(`${env.parsed["DEFAULT_PATH"]}/build`)
+    .setPublicPath('/build')
 
     /*
      * ENTRY CONFIG
