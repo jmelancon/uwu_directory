13f201d62d5495324f72171c790395e5bf84e1fc
Joseph Melancon <joseph@jmelancon.com>
2025-02-14 05:57:49 +0000

    make samba provision actually work correctly

--- config/smblds/entrypoint.d/provision.sh
+++ config/smblds/entrypoint.d/provision.sh
@@ -1,30 +1,22 @@
 #!/bin/sh
 
-export BASE_DN=$( \
-        echo $REALM | \
-        tr "[:lower:]" "[:upper:]" | \
-        sed 's:\.:,DC=:g' | \
-        sed 's/^/DC=/' \
-    );
-
-UUID_SRC="/proc/sys/kernel/random/uuid"
-GROUP_GUID=$(cat $UUID_SRC)
-SERVICE_GUID=$(cat $UUID_SRC)
-BASIC_USERS_GUID=$(cat $UUID_SRC)
-SSO_ADMIN_GUID=$(cat $UUID_SRC)
-
 if [ ! -f /etc/samba/provisioned ]; then
-  cat /entrypoint.d/provision.ldif | \
-   sed "s/%DN_HERE%/$BASE_DN/" | \
-   sed "s/%GROUP_GUID_HERE%/$GROUP_GUID/" | \
-   sed "s/%SERVICE_GUID_HERE%/$SERVICE_GUID/" |\
-   sed "s/%BASIC_USERS_GUID_HERE%/$BASIC_USERS_GUID/" | \
-   sed "s/%SSO_ADMIN_GUID_HERE%/$SSO_ADMIN_GUID/" > /tmp/provision.ldif;
-  ldbmodify -H "/var/lib/samba/private/sam.ldb.d/$BASE_DN.ldb" /tmp/provision.ldif && \
-  echo "uwu you're so provisioned swaggy :3" > /etc/samba/provisioned;
-  echo "Directory is hopefully provisioned.......";
-  samba-tool user setexpiry Administrator --noexpiry
-  samba-tool domain passwordsettings set --max-pwd-age=0
+  samba-tool ou create OU=Services;
+  samba-tool ou create OU=Groups;
+  samba-tool group create "SSO Administrators" --groupou OU=Groups;
+  samba-tool group create "Basic Users" --groupou OU=Groups;
+
+  if samba-tool dbcheck; then
+    echo "uwu you're so provisioned swaggy :3" > /etc/samba/provisioned;
+    echo "directory's provisioned, bosshog"
+  else
+    echo "uh ohhhhhhhh, shit's broken :(";
+    exit 1;
+  fi
+
+  # Set a forgiving expiry on accounts. I don't want to change my password!
+  samba-tool user setexpiry Administrator --noexpiry;
+  samba-tool domain passwordsettings set --max-pwd-age=0;
 fi
 
 exit 0;
