6bf9d22b113f72e09a8245dcc65d1099bf9a5c6a
Joseph Melancon <joseph@jmelancon.com>
2025-02-25 12:35:36 +0000

    i'm going to bed i can't do this

--- server/assets/typescript/url_parser.ts
+++ server/assets/typescript/url_parser.ts
@@ -1,4 +1,5 @@
 let rootUrl: string;
+let basePath: string = "";
 
 /**
  * Take a relative URL and make it absolute so reverse proxies work.
@@ -10,10 +11,13 @@ let rootUrl: string;
  * The constructed URL.
  */
 export function parseUrl(relative: string): string {
-    return rootUrl + relative;
+    return basePath + relative;
 }
 window.addEventListener("load",(_e) => {
     // We need the root URL of the site so our stuff works when behind a reverse proxy. A reference
     // to the URL is hidden in the site's <header>.
     rootUrl = document.getElementById("root_url")?.getAttribute("href") ?? "";
+    if (rootUrl) {
+        basePath = new URL(rootUrl).pathname ?? "";
+    }
 });
